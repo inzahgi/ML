138a7b1e01340bfd8cd02d665db1897c377250e1
inzahgi <inzahgi@126.com>
2019-02-28 21:24:32 +0800

    add 06

--- test/hand_on_ML/chapter6/test6.py
+++ test/hand_on_ML/chapter6/test6.py
@@ -212,58 +212,59 @@ if __name__ == '__main__':
     save_fig("sensitivity_to_rotation_plot")
     plt.show()
 
-#     # Quadratic training set + noise
-#     np.random.seed(42)
-#     m = 200
-#     X = np.random.rand(m, 1)
-#     y = 4 * (X - 0.5) ** 2
-#     y = y + np.random.randn(m, 1) / 10
-#
-#     tree_reg = DecisionTreeRegressor(max_depth=2)
-#     tree_reg.fit(X, y)
-#
-#     export_graphviz(
-#         tree_reg,
-#         out_file=image_path("regression_tree.dot"),
-#         feature_names=["x1"],
-#         rounded=True,
-#         filled=True
-#     )
-#
-#     Image(filename=
-#           os.path.dirname(os.path.realpath(__file__)) +
-#           "../images/06_Decision Trees/regression_tree.png",
-#           width=400, height=400, )
-#
-#     tree_reg1 = DecisionTreeRegressor(random_state=42, max_depth=2)
-#     tree_reg2 = DecisionTreeRegressor(random_state=42, max_depth=3)
-#     tree_reg1.fit(X, y)
-#     tree_reg2.fit(X, y)
-#
-#     plt.figure(figsize=(11, 4))
-#     plt.subplot(121)
-#
-#     plot_regression_predictions(tree_reg1, X, y)
-#     for split, style in ((0.1973, "k-"), (0.0917, "k--"), (0.7718, "k--")):
-#         plt.plot([split, split], [-0.2, 1], style, linewidth=2)
-#     plt.text(0.21, 0.65, "Depth=0", fontsize=15)
-#     plt.text(0.01, 0.2, "Depth=1", fontsize=13)
-#     plt.text(0.65, 0.8, "Depth=1", fontsize=13)
-#     plt.legend(loc="upper center", fontsize=18)
-#     plt.title("max_depth=2", fontsize=14)
-#
-#     plt.subplot(122)
-#
-#     plot_regression_predictions(tree_reg2, X, y, ylabel=None)
-#     for split, style in ((0.1973, "k-"), (0.0917, "k--"), (0.7718, "k--")):
-#         plt.plot([split, split], [-0.2, 1], style, linewidth=2)
-#     for split in (0.0458, 0.1298, 0.2873, 0.9040):
-#         plt.plot([split, split], [-0.2, 1], "k:", linewidth=1)
-#     plt.text(0.3, 0.5, "Depth=2", fontsize=13)
-#     plt.title("max_depth=3", fontsize=14)
-#
-#     save_fig("tree_regression_plot")
-#     plt.show()
+##  决策树回归
+    # Quadratic training set + noise
+    np.random.seed(42)
+    m = 200
+    X = np.random.rand(m, 1)
+    y = 4 * (X - 0.5) ** 2
+    y = y + np.random.randn(m, 1) / 10 ## 引入噪声
+
+    tree_reg = DecisionTreeRegressor(max_depth=2)
+    tree_reg.fit(X, y)
+
+    export_graphviz(
+        tree_reg,
+        out_file=image_path("regression_tree.dot"),
+        feature_names=["x1"],
+        rounded=True,
+        filled=True
+    )
+
+    Image(filename=
+          os.path.dirname(os.path.realpath(__file__)) +
+          "../images/06_Decision Trees/regression_tree.png",
+          width=400, height=400, )
+
+    tree_reg1 = DecisionTreeRegressor(random_state=42, max_depth=2)
+    tree_reg2 = DecisionTreeRegressor(random_state=42, max_depth=3)
+    tree_reg1.fit(X, y)
+    tree_reg2.fit(X, y)
+
+    plt.figure(figsize=(11, 4))
+    plt.subplot(121)  ## 树深度为二
+
+    plot_regression_predictions(tree_reg1, X, y)
+    for split, style in ((0.1973, "k-"), (0.0917, "k--"), (0.7718, "k--")):
+        plt.plot([split, split], [-0.2, 1], style, linewidth=2)
+    plt.text(0.21, 0.65, "Depth=0", fontsize=15)
+    plt.text(0.01, 0.2, "Depth=1", fontsize=13)
+    plt.text(0.65, 0.8, "Depth=1", fontsize=13)
+    plt.legend(loc="upper center", fontsize=18)
+    plt.title("max_depth=2", fontsize=14)
+
+    plt.subplot(122)  ## 树的深度为三
+
+    plot_regression_predictions(tree_reg2, X, y, ylabel=None)
+    for split, style in ((0.1973, "k-"), (0.0917, "k--"), (0.7718, "k--")):
+        plt.plot([split, split], [-0.2, 1], style, linewidth=2)
+    for split in (0.0458, 0.1298, 0.2873, 0.9040):
+        plt.plot([split, split], [-0.2, 1], "k:", linewidth=1)
+    plt.text(0.3, 0.5, "Depth=2", fontsize=13)
+    plt.title("max_depth=3", fontsize=14)
+
+    save_fig("tree_regression_plot")
+    plt.show()
 #
 # ## cart算法
 #     tree_reg1 = DecisionTreeRegressor(random_state=42)
