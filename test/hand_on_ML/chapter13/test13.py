07788df3a342f8c7f82a4bb588ea194c818a7c3e
inzahgi <inzahgi@126.com>
2019-03-29 22:11:07 +0800

    add 13

--- test/hand_on_ML/chapter13/test13.py
+++ test/hand_on_ML/chapter13/test13.py
@@ -82,4 +82,53 @@ if __name__ == '__main__':
     save_fig("china_horizontal", tight_layout=False)
     plt.show()
 
+
 ## Stacking Multiple Feature Maps
+    # Load sample images
+    china = load_sample_image("china.jpg")
+    flower = load_sample_image("flower.jpg")
+    dataset = np.array([china, flower], dtype=np.float32)
+    batch_size, height, width, channels = dataset.shape
+
+    # Create 2 filters
+    filters = np.zeros(shape=(7, 7, channels, 2), dtype=np.float32)
+    filters[:, 3, :, 0] = 1  # vertical line
+    filters[3, :, :, 1] = 1  # horizontal line
+
+    # Create a graph with input X plus a convolutional layer applying the 2 filters
+    X = tf.placeholder(tf.float32, shape=(None, height, width, channels))
+    convolution = tf.nn.conv2d(X, filters, strides=[1, 2, 2, 1], padding="SAME")
+
+    with tf.Session() as sess:
+        output = sess.run(convolution, feed_dict={X: dataset})
+
+    plt.imshow(output[0, :, :, 1], cmap="gray")  # plot 1st image's 2nd feature map
+    plt.show()
+
+    for image_index in (0, 1):
+        for feature_map_index in (0, 1):
+            plot_image(output[image_index, :, :, feature_map_index])
+            plt.show()
+
+    reset_graph()
+
+    X = tf.placeholder(shape=(None, height, width, channels), dtype=tf.float32)
+    conv = tf.layers.conv2d(
+        X,
+        filters=2,
+        kernel_size=7,
+        strides=[2, 2],
+        padding="SAME"
+    )
+
+    init = tf.global_variables_initializer()
+
+    with tf.Session() as sess:
+        init.run()
+        output = sess.run(conv, feed_dict={X: dataset})
+
+    plt.imshow(output[0, :, :, 1], cmap="gray")  # 绘制第一张图像的第二张特征图
+    plt.show()
+
+
+## VALID vs SAME padding
