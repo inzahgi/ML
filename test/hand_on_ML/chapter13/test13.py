c505f87b6f17ee1c1146c8ae7007269fae7bc978
inzahgi <inzahgi@126.com>
2019-04-23 22:46:20 +0800

    add 13

--- test/hand_on_ML/chapter13/test13.py
+++ test/hand_on_ML/chapter13/test13.py
@@ -130,5 +130,136 @@ if __name__ == '__main__':
 #     plt.imshow(output[0, :, :, 1], cmap="gray")  # 绘制第一张图像的第二张特征图
 #     plt.show()
 #
-
-## VALID vs SAME padding
+#
+# ## VALID vs SAME padding
+# ########################################
+#     reset_graph()
+#
+#     filter_primes = np.array([2., 3., 5., 7., 11., 13.], dtype=np.float32)
+#     x = tf.constant(np.arange(1, 13 + 1, dtype=np.float32).reshape([1, 1, 13, 1]))
+#     filters = tf.constant(filter_primes.reshape(1, 6, 1, 1))
+#
+#     valid_conv = tf.nn.conv2d(x, filters, strides=[1, 1, 5, 1], padding='VALID')
+#     same_conv = tf.nn.conv2d(x, filters, strides=[1, 1, 5, 1], padding='SAME')
+#
+#     with tf.Session() as sess:
+#         print("VALID:\n", valid_conv.eval())
+#         print("SAME:\n", same_conv.eval())
+#
+#     print("VALID:")
+#     print(np.array([1, 2, 3, 4, 5, 6]).T.dot(filter_primes))
+#     print(np.array([6, 7, 8, 9, 10, 11]).T.dot(filter_primes))
+#     print("SAME:")
+#     print(np.array([0, 1, 2, 3, 4, 5]).T.dot(filter_primes))
+#     print(np.array([5, 6, 7, 8, 9, 10]).T.dot(filter_primes))
+#     print(np.array([10, 11, 12, 13, 0, 0]).T.dot(filter_primes))
+#
+#
+# #####  Pooling layer - 池化层
+#     batch_size, height, width, channels = dataset.shape
+#
+#     filters = np.zeros(shape=(7, 7, channels, 2), dtype=np.float32)
+#     filters[:, 3, :, 0] = 1  # vertical line
+#     filters[3, :, :, 1] = 1  # horizontal line
+#
+#     # Create a graph with input X plus a max pooling layer
+#     X = tf.placeholder(tf.float32, shape=(None, height, width, channels))
+#     max_pool = tf.nn.max_pool(X, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding="VALID")
+#
+#     with tf.Session() as sess:
+#         output = sess.run(max_pool, feed_dict={X: dataset})
+#
+#     plt.imshow(output[0].astype(np.uint8))  # plot the output for the 1st image
+#     plt.show()
+#
+#     plot_color_image(dataset[0])
+#     save_fig("china_original")
+#     plt.show()
+#
+#     plot_color_image(output[0])
+#     save_fig("china_max_pool")
+#     plt.show()
+#
+#
+# ##### CNN Architectures - CNN架构
+#
+# #### MNIST
+#
+#     height = 28
+#     width = 28
+#     channels = 1
+#     n_inputs = height * width
+#
+#     conv1_fmaps = 32
+#     conv1_ksize = 3
+#     conv1_stride = 1
+#     conv1_pad = "SAME"
+#
+#     conv2_fmaps = 64
+#     conv2_ksize = 3
+#     conv2_stride = 2
+#     conv2_pad = "SAME"
+#
+#     pool3_fmaps = conv2_fmaps
+#
+#     n_fc1 = 64
+#     n_outputs = 10
+#
+#     reset_graph()
+#
+#     with tf.name_scope("inputs"):
+#         X = tf.placeholder(tf.float32, shape=[None, n_inputs], name="X")
+#         X_reshaped = tf.reshape(X, shape=[-1, height, width, channels])
+#         y = tf.placeholder(tf.int32, shape=[None], name="y")
+#
+#     conv1 = tf.layers.conv2d(X_reshaped, filters=conv1_fmaps, kernel_size=conv1_ksize,
+#                              strides=conv1_stride, padding=conv1_pad,
+#                              activation=tf.nn.relu, name="conv1")
+#     conv2 = tf.layers.conv2d(conv1, filters=conv2_fmaps, kernel_size=conv2_ksize,
+#                              strides=conv2_stride, padding=conv2_pad,
+#                              activation=tf.nn.relu, name="conv2")
+#
+#     with tf.name_scope("pool3"):
+#         pool3 = tf.nn.max_pool(conv2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding="VALID")
+#         pool3_flat = tf.reshape(pool3, shape=[-1, pool3_fmaps * 7 * 7])
+#
+#     with tf.name_scope("fc1"):
+#         fc1 = tf.layers.dense(pool3_flat, n_fc1, activation=tf.nn.relu, name="fc1")
+#
+#     with tf.name_scope("output"):
+#         logits = tf.layers.dense(fc1, n_outputs, name="output")
+#         Y_proba = tf.nn.softmax(logits, name="Y_proba")
+#
+#     with tf.name_scope("train"):
+#         xentropy = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logits, labels=y)
+#         loss = tf.reduce_mean(xentropy)
+#         optimizer = tf.train.AdamOptimizer()
+#         training_op = optimizer.minimize(loss)
+#
+#     with tf.name_scope("eval"):
+#         correct = tf.nn.in_top_k(logits, y, 1)
+#         accuracy = tf.reduce_mean(tf.cast(correct, tf.float32))
+#
+#     with tf.name_scope("init_and_save"):
+#         init = tf.global_variables_initializer()
+#         saver = tf.train.Saver()
+#
+#     from tensorflow.examples.tutorials.mnist import input_data
+#
+#     mnist = input_data.read_data_sets("/tmp/data/")
+#
+#     n_epochs = 10
+#     batch_size = 100
+#
+#     with tf.Session() as sess:
+#         init.run()
+#         for epoch in range(n_epochs):
+#             for iteration in range(mnist.train.num_examples // batch_size):
+#                 X_batch, y_batch = mnist.train.next_batch(batch_size)
+#                 sess.run(training_op, feed_dict={X: X_batch, y: y_batch})
+#             acc_train = accuracy.eval(feed_dict={X: X_batch, y: y_batch})
+#             acc_test = accuracy.eval(feed_dict={X: mnist.test.images, y: mnist.test.labels})
+#             print(epoch, "Train accuracy:", acc_train, "Test accuracy:", acc_test)
+#
+#             save_path = saver.save(sess, "./my_mnist_model")
+#
