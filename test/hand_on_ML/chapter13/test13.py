3f3e34f8795e234bd78c23989da18e0c99ed771a
inzahgi <inzahgi@126.com>
2019-05-20 22:48:07 +0800

    add 13

--- test/hand_on_ML/chapter13/test13.py
+++ test/hand_on_ML/chapter13/test13.py
@@ -46,20 +46,20 @@ def plot_color_image(image):
 
 
 if __name__ == '__main__':
-#     china = load_sample_image("china.jpg")
-#     flower = load_sample_image("flower.jpg")
-#     image = china[150:220, 130:250]
-#     height, width, channels = image.shape
-#     image_grayscale = image.mean(axis=2).astype(np.float32)
-#     images = image_grayscale.reshape(1, height, width, 1)
-#
-#     fmap = np.zeros(shape=(7, 7, 1, 2), dtype=np.float32)
-#     fmap[:, 3, 0, 0] = 1
-#     fmap[3, :, 0, 1] = 1
-#     plot_image(fmap[:, :, 0, 0])
-#     plt.show()
-#     plot_image(fmap[:, :, 0, 1])
-#     plt.show()
+    china = load_sample_image("china.jpg")
+    flower = load_sample_image("flower.jpg")
+    image = china[150:220, 130:250]
+    height, width, channels = image.shape
+    image_grayscale = image.mean(axis=2).astype(np.float32)
+    images = image_grayscale.reshape(1, height, width, 1)
+
+    fmap = np.zeros(shape=(7, 7, 1, 2), dtype=np.float32)
+    fmap[:, 3, 0, 0] = 1
+    fmap[3, :, 0, 1] = 1
+    plot_image(fmap[:, :, 0, 0])
+    plt.show()
+    plot_image(fmap[:, :, 0, 1])
+    plt.show()
 #
 #     reset_graph()
 #
